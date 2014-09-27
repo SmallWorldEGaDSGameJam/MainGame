4b79a4e91e5f7c6642c9bd0e8121c98db9949b44
ramkotta <macbookowner@wireless-10-147-212-97.public.utexas.edu>
2014-09-26 22:21:31 -0500

    Platform correction
    Added parameters to rect

--- src/MainGame/Platform.java
+++ src/MainGame/Platform.java
@@ -7,11 +7,11 @@ import AppletSource.Utilities.GameRectangle;
 import AppletSource.Utilities.Vector2;
 
 public class Platform extends GameObject {
-	GameRectangle rect;
+	private GameRectangle rect;
 	
 	public Platform(Image image, Vector2 initialPos) {
 		super(image, initialPos);
-		rect = new GameRectangle();
+		rect = new GameRectangle(initialPos.x, initialPos.y, image.getWidth(null),image.getHeight(null) );
 	}
 	
 	public GameRectangle getRectangle() {
