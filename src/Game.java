56b9d770d3617b01eadc8ffc3816629b4bc7780e
Ricardo Delfin <ricardo.delfin@utexas.edu>
2014-09-26 21:24:59 -0500

    Initial Base Game Code
    Base code with the Game class

---
+++ src/Game.java
@@ -0,0 +1,32 @@
+import java.awt.Graphics2D;
+
+import AppletSource.AppletCore;
+import AppletSource.GameTime;
+
+public class Game extends AppletCore {
+
+	@Override
+	public void resizeScreen() {
+		resize(1080, 720);
+		
+	}
+
+	@Override
+	public void LoadContent() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void Update(GameTime arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void Draw(Graphics2D arg0, GameTime arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
